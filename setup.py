95ea297580a17295d909969ea2ce9f4b6ded232f
Miguel Ike Dumancas <ikedumancas@gmail.com>
2019-11-01 02:21:28 +0800

    make boto3 a dependency of this app

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name="UpDwn",
     version="1.0",
     py_modules=['updwn'],
-    install_requires=['Click'],
+    install_requires=['Click', 'boto3'],
     entry_points={'console_scripts': 'updwn=updwn:main'}
 )
