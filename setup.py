eefa3eb041ef562c2aa6537543e33e652bfc44de
Miguel Ike Dumancas <ikedumancas@gmail.com>
2019-11-01 03:05:08 +0800

    make updwn as a package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="UpDwn",
     version="1.0",
-    py_modules=['updwn'],
+    packages=['updwn'],
     install_requires=['Click', 'boto3'],
-    entry_points={'console_scripts': 'updwn=updwn:main'}
+    entry_points={'console_scripts': 'updwn=updwn.updwn:main'}
 )
